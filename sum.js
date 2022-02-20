c80c789ae3183bfdadf0f6faf7d924aae2423a35
SQZ777 <sqz777isme@gmail.com>
2022-02-20 23:55:31 +0800

    fix pre commit config yaml

--- sum.js
+++ sum.js
@@ -1,4 +1,10 @@
-function sum(a, b) {
-  return a + b;
+/**
+ * Add two numbers.
+ * @param {number} num1 The first number.
+ * @param {number} num2 The second number.
+ * @return {number} The sum of the two numbers.
+ */
+function sum(num1, num2) {
+  return num1 + num2;
 }
 module.exports = sum;
