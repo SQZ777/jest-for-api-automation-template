6ecd56ee876f602062a7fd64b96861bdce103ae2
SQZ777 <sqz777isme@gmail.com>
2022-02-23 12:00:27 +0800

    add 3rd parmaeter

--- test-folder/sum3.js
+++ test-folder/sum3.js
@@ -2,9 +2,10 @@
  * Add two numbers.
  * @param {number} num1 The first number.
  * @param {number} num2 The second number.
+ * @param {number} num3 The third number.
  * @return {number} The sum of the two numbers.
  */
-function sum3(num1, num2) {
-  return num1 + num2;
+function sum3(num1, num2, num3) {
+  return num1 + num2 + num3;
 }
 module.exports = sum3;
