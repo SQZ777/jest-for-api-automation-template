dca8f50a93957679c1bf87268dd78703179fb357
darren_zhang <darren_zhang@trendmicro.com>
2022-03-06 22:38:06 +0800

    add throw when CONFIG_ENV has not set

--- config/index.js
+++ config/index.js
@@ -1,6 +1,10 @@
 const _ = require('lodash');
 const defaults = require('./defaults');
 
+if (!_.has(process.env, 'CONFIG_ENV')) {
+  throw Error('CONFIG_ENV should set in CLI');
+}
+
 const config = _.merge({}, require('./envs/prod.js'), defaults);
 
 module.exports = config;
