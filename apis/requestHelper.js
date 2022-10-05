9021bc8742dbce516758f62e22677b3c2d4a4502
darren_zhang <darren_zhang@trendmicro.com>
2022-10-05 23:21:17 +0800

    update requestHelper

--- apis/requestHelper.js
+++ apis/requestHelper.js
@@ -24,14 +24,12 @@ async function postRequest(baseURL, headers, data) {
 /**
  * @param {string} baseURL for base URL
  * @param {object} headers for request headers
- * @param {object} data for request payload
  */
-async function getRequest(baseURL, headers, data) {
+async function getRequest(baseURL, headers) {
   const result = await axios({
     method: 'get',
     url: baseURL,
     headers: headers,
-    data: data,
   })
       .then((result) => {
         return result;
