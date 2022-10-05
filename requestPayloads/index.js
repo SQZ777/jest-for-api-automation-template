cc54761c4469534c9ce85aa9a559c3835b49a3b5
darren_zhang <darren_zhang@trendmicro.com>
2022-10-06 00:52:35 +0800

    add requestPayloads for products

---
+++ requestPayloads/index.js
@@ -0,0 +1,5 @@
+const products = require('./productsRequests');
+
+module.exports = {
+  products,
+};
---
+++ requestPayloads/productsRequests.js
@@ -0,0 +1,16 @@
+const create = {
+  productId: 4,
+  productName: 'Phone',
+  productPrice: 356,
+};
+
+const update = {
+  productId: 1,
+  productName: 'Cable',
+  productPrice: 1223,
+};
+
+module.exports = {
+  update,
+  create,
+};
