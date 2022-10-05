930ac1cff1ebd9e0cf12cbf33f527c78c10fbedc
darren_zhang <darren_zhang@trendmicro.com>
2022-10-05 23:15:04 +0800

    add requestHelper

---
+++ apis/requestHelper.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+/**
+ * @param {string} baseURL for base URL
+ * @param {object} headers for request headers
+ * @param {object} data for request payload
+ */
+async function postRequest(baseURL, headers, data) {
+  const result = await axios({
+    method: 'post',
+    url: baseURL,
+    headers: headers,
+    data: data,
+  })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  return result;
+}
+
+/**
+ * @param {string} baseURL for base URL
+ * @param {object} headers for request headers
+ * @param {object} data for request payload
+ */
+async function getRequest(baseURL, headers, data) {
+  const result = await axios({
+    method: 'get',
+    url: baseURL,
+    headers: headers,
+    data: data,
+  })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  return result;
+}
+
+module.exports = {
+  postRequest,
+  getRequest,
+};
